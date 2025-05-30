7f5aa886a2740f9f4e0ea53066d19696a4c4b242
Friox <devfriox@gmail.com>
2025-05-30 10:48:39 +0900

    init

---
+++ src/main/java/dev/friox/backend/service/CalculatorService.java
@@ -0,0 +1,13 @@
+package dev.friox.backend.service;
+
+import dev.friox.backend.dto.CalculatorDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+    public int plusTwoNumbers(CalculatorDTO calculatorDTO) {
+
+        return calculatorDTO.getNum1() + calculatorDTO.getNum2();
+    }
+}
